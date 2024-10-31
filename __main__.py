34b4590ab44d850dc22ee1be0fdc9a095c18912f
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-10-31 16:48:08 -0400

    Feat: larger astroids not split into two when shot

--- __main__.py
+++ __main__.py
@@ -46,7 +46,7 @@ def main():
                 sys.exit()
             for shot in shots:
                 if asteroid.collides(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         for drawable in drawables:
