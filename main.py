bddcdc4397dc6447468773c66a718a45c16d8361
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-11-11 10:22:29 -0500

    Reestablished game loop

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import pygame
+from lib.constants import *
 
 # game initialization
 pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Asteroids")
+running = True
+
+# game loop
+while running:
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    # draw
+    screen.fill("black")
+    # update
+    pygame.display.update()
