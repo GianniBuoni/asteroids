ff47afbddcab4a8df7c6f4a19623f9aec2f097c7
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-11-11 11:44:17 -0500

    Added delta time / framerate lock

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import pygame
-from lib.constants import *
+from lib.constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 # game initialization
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Asteroids")
+clock = pygame.time.Clock()
 running = True
 
 # game loop
 while running:
+    dt = clock.tick(60) / 1000
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     # draw
-    screen.fill("black")
+    screen.fill("grey9")
     # update
     pygame.display.update()
